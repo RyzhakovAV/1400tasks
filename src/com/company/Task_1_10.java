324afc976b51b099d62a23d362cbef9c834f53a7
RyzhakovAV <rjizhakov@mail.ru>
2023-04-06 19:51:40 +0500

    add new task input firstname from keyboard

---
+++ src/com/company/Task_1_10.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Scanner;
+public class Task_1_10 {
+    public static void main(String[] args){
+        Scanner console = new Scanner(System.in);
+        String name = console.nextLine();
+        System.out.println(name);
+    }
+}
