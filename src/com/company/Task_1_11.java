7cd5f18dbfc26e329abb7c479ec4f6e626287d64
RyzhakovAV <rjizhakov@mail.ru>
2023-04-06 19:59:05 +0500

    add new task input from keyboard

---
+++ src/com/company/Task_1_11.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+public class Task_1_11 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String team = console.nextLine();
+        String message = " – это чемпион!";
+        System.out.println(team + message);
+    }
+}
