79a00c4a3a588a488de8267e1b6d5fc98bb33ae1
RyzhakovAV <rjizhakov@mail.ru>
2023-04-14 20:24:03 +0500

    add new task input numbers

--- src/com/company/Task_1_14.java
+++ src/com/company/Task_1_14.java
@@ -4,11 +4,9 @@ import java.util.Scanner;
 public class Task_1_14 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        int number = console.nextInt();
-        int numberAfter = number + 1;
-        int numberBefore = number - 1;
-        System.out.printf("Следующее за числом %d число – %d.\n", number, numberAfter);
-        System.out.printf("Для числа %d предыдущее число – %d.\n", number,numberBefore);
-
+        int numberOne = console.nextInt();
+        int numberTwo = console.nextInt();
+        int numberThree = console.nextInt();
+        System.out.printf("%d  %d  %d", numberOne, numberTwo, numberThree);
     }
 }
