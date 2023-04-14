bd2397814d40c5678ed1f326e8b94d5c6e3049df
RyzhakovAV <rjizhakov@mail.ru>
2023-04-14 22:47:51 +0500

    add new task cycle

---
+++ src/com/company/Task_1_15.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+public class Task_1_15 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int inputNumber = 0;
+        int[] arrayNumbers = new int[4];
+
+        for(int i = 0; i < arrayNumbers.length; i++) {
+            inputNumber = console.nextInt();
+            arrayNumbers[i] = inputNumber;
+        }
+
+        for (int i = 0; i < arrayNumbers.length; i++) {
+            System.out.print(arrayNumbers[i] + " ");
+        }
+    }
+}
