044faa5a59b800b1dce4e1b30820a11b0b169e3a
RyzhakovAV <rjizhakov@mail.ru>
2023-05-01 09:24:40 +0500

    task about math

---
+++ src/com/company/Task_2_02.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Task_2_02 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Введите значение а: ");
+        String a = console.next();
+        float number = Float.parseFloat(a);
+        double equals = (Math.pow(number, 2) + 10) / (Math.sqrt(Math.pow(number, 2) + 1));
+        System.out.println(equals);
+    }
+}
