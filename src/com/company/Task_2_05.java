a278c341d8030b5e7bf03f01522e3b371b089a83
RyzhakovAV <rjizhakov@mail.ru>
2023-05-01 10:06:19 +0500

    task radius

---
+++ src/com/company/Task_2_05.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+public class Task_2_05 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        try {
+            System.out.print("Введите радиус окружности: ");
+            double R = console.nextDouble();
+            System.out.printf("Диаметр равен %.2f при радиусе равном %.2f", R*2, R);
+        }catch (Exception e) {
+            System.out.println("Ошибка ввода");
+        }
+    }
+}
