3ebc6b3d05070c1efba223c87f3c5bb3ec140e0e
RyzhakovAV <rjizhakov@mail.ru>
2023-05-02 10:04:37 +0500

    people in country

---
+++ src/com/company/Task_2_12.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Task_2_12 {
+    public static void main(String[] args) {
+        int people = 146_424_729;
+        int S = 17_100_000;
+        double p = people / S;
+        System.out.println("Плотность населения в России: " + p + " человек на км^2");
+    }
+}
