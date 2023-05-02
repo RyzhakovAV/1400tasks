df16c408a45473c1a657713ac8c078ce862a533a
RyzhakovAV <rjizhakov@mail.ru>
2023-05-02 09:43:30 +0500

    po

---
+++ src/com/company/Task_2_11.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Task_2_11 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Введите объем в см^3: ");
+        String inputV = console.next();
+        System.out.print("Введите массу в граммах: ");
+        String inputM = console.next();
+
+        try {
+            double V = Double.parseDouble(inputV);
+            double M = Double.parseDouble(inputM);
+
+            System.out.printf("Плотность равна: %.2f гр/см^3", M / V);
+        }catch(Exception e) {
+
+        }
+    }
+}
