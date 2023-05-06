2a03e3d779e960d6701e1fddebea2e4c7243c851
RyzhakovAV <rjizhakov@mail.ru>
2023-05-06 10:43:01 +0500

    P triangle

--- src/com/company/Task_2_16.java
+++ src/com/company/Task_2_16.java
@@ -5,21 +5,22 @@ import java.util.Scanner;
 public class Task_2_16 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-
-        System.out.print("Введите высоту трапеции h: ");
-        String inputH = console.next();
-        System.out.print("Введите длину основания трапеции a: ");
+        System.out.print("Введите катет a = ");
         String inputA = console.next();
-        System.out.print("Введите длину основания трапеции b: ");
+        System.out.print("Введите катет b = ");
         String inputB = console.next();
+
         try {
-            double h = Math.abs(Double.parseDouble(inputH));
-            double a = Math.abs(Double.parseDouble(inputA));
-            double b = Math.abs(Double.parseDouble(inputB));
-            double S = ((a + b) / 2) * h;
-            double P = a + b + 2 * Math.sqrt(Math.pow(h, 2) + Math.pow(a - b, 2) / 4);
-            System.out.printf("Площадь равнобедренной трапеции равна %.2f", S);
-            System.out.printf("\nПериметр равнобедренной трапеции равна %.2f", P);
+            double a = Double.parseDouble(inputA);
+            double b = Double.parseDouble(inputB);
+            if (a <= 0 || b <=0) {
+                System.out.println("Сторона треугольника не может быть меньше или равна нулю");
+            }else {
+                double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+                double P = a + b + c;
+                System.out.printf("Длина катета a = %s. Длина катета b = %s. Длина гипотенузы c = %.2f. " +
+                        "Площадь прямоугольного треугольника = %.2f", inputA, inputB, c, P);
+            }
         }catch (Exception e) {
             System.out.println("Ошибка ввода");
         }
