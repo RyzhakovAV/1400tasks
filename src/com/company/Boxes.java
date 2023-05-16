b1b13193d8188114ae5de940afc3d71b11a09ca7
RyzhakovAV <rjizhakov@mail.ru>
2023-05-16 10:44:19 +0500

    task 100 boxes

--- src/com/company/Boxes.java
+++ src/com/company/Boxes.java
@@ -6,8 +6,12 @@ public class Boxes {
     static int[] arrayBoxes = new int[100];
     static int[] arrayPeople = new int[100];
     static Random rnd = new Random();
-    static int sumFind = 0;
+    static int sumFindOrder = 0;
+    static int sumFindRandom = 0;
     static int resultOrder = 0;
+    static int resultRandom = 0;
+    static int sumFirstRandom = 0;
+    static int resultFirstRandom = 0;
 
     public static void fillArray(int[] array) {
         for(int i = 0; i < array.length; i++) {
@@ -42,6 +46,27 @@ public class Boxes {
         }
         return 0;
     }
+
+    public static int findBoxFirstRandom(int[] array, int man) {
+        int ticket = rnd.nextInt(0,100);;
+        for(int i = 1; i < 50; i ++) {
+            if (man == array[ticket]) {
+                return 1;
+            }else{
+                ticket = array[ticket];
+            }
+        }
+        return 0;
+    }
+
+    public static int findBoxRandom(int[] array, int man) {
+        for(int i = 0; i < 50; i ++) {
+            int ticket = rnd.nextInt(0,100);
+            if (man == array[ticket]) return 1;
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
 
         fillArray(arrayBoxes);
@@ -50,19 +75,50 @@ public class Boxes {
         for(int i = 0; i < 1_000_000; i++) {
             mixArray(arrayBoxes);
 
-            sumFind = 0;
+            sumFindOrder = 0;
             for(int j = 0; j < arrayPeople.length; j++) {
-                sumFind += findBoxOrder(arrayBoxes, j);
-                if(sumFind == 100) {
+                sumFindOrder += findBoxOrder(arrayBoxes, j);
+                if(sumFindOrder == 100) {
                     resultOrder++;
                 }
             }
-            System.out.println(sumFind);
+            System.out.println(sumFindOrder);
         }
 
+        for(int i = 0; i < 1_000_000; i++) {
+            mixArray(arrayBoxes);
+
+            sumFindRandom = 0;
+            for(int j = 0; j < arrayPeople.length; j++) {
+                sumFindRandom += findBoxRandom(arrayBoxes, j);
+                if(sumFindRandom == 100) {
+                    resultRandom++;
+                }
+            }
+            System.out.println(sumFindRandom);
+        }
+
+        for(int i = 0; i < 1_000_000; i++) {
+            mixArray(arrayBoxes);
+
+            sumFirstRandom = 0;
+            for(int j = 0; j < arrayPeople.length; j++) {
+                sumFirstRandom += findBoxFirstRandom(arrayBoxes, j);
+                if(sumFirstRandom == 100) {
+                    resultFirstRandom++;
+                }
+            }
+            System.out.println(sumFirstRandom);
+        }
 
         System.out.println("Результат выживания при условии что заключенный открывает первую коробку со своим номером");
         resultOrder *= 0.0001;
         System.out.println("и по номеру в коробке открывает следующию : " + resultOrder + " %");
+        System.out.println("Результат выживания при условии что заключенный открывает случайную коробку");
+        resultRandom *= 0.0001;
+        System.out.println(resultRandom + " %");
+        System.out.println("Результат выживания при условии что заключенный открывает первую случайную коробку");
+        resultFirstRandom *= 0.0001;
+        System.out.println("и по номеру в коробке открывает следующию : " + resultFirstRandom + " %");
     }
 }
