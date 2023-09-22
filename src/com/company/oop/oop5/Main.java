b0606565e0b24b0879d11dbe6a6fa3f1d1725935
RyzhakovAV <rjizhakov@mail.ru>
2023-09-22 12:11:10 +0500

    oop, constructor and reboot methods

--- src/com/company/oop/oop5/Main.java
+++ src/com/company/oop/oop5/Main.java
@@ -1,2 +1,16 @@
-package com.company.oop.oop5;public class Main {
+package com.company.oop.oop5;
+
+public class Main {
+    public static void main(String[] args) {
+        Monstor monstor = new Monstor();
+        Monstor monstor1 = new Monstor(3);
+        Monstor monstor2 = new Monstor(2, 4);
+        Monstor monstor3 = new Monstor(2, 2, 4);
+
+        monstor.voice();
+        System.out.println();
+        monstor1.voice(5);
+        monstor2.voice(2);
+        monstor3.voice(4, "arrrh");
+    }
 }
--- src/com/company/oop/oop5/Monstor.java
+++ src/com/company/oop/oop5/Monstor.java
@@ -1,2 +1,49 @@
-package com.company.oop.oop5;public class Monstor {
+package com.company.oop.oop5;
+
+public class Monstor {
+    int eyes;
+    int legs;
+    int hands;
+
+    public Monstor() {
+        eyes = 2;
+        legs = 2;
+        hands = 2;
+    }
+
+    public Monstor(int eyes) {
+        this.eyes = eyes;
+        legs = 2;
+        hands = 2;
+    }
+
+    public Monstor(int eyes, int legs) {
+        this.eyes = eyes;
+        this.legs = legs;
+        hands = 2;
+    }
+
+    public Monstor(int eyes, int legs, int hands) {
+        this.eyes = eyes;
+        this.legs = legs;
+        this.hands = hands;
+    }
+
+    public void voice() {
+        System.out.print("голос ");
+    }
+
+    public void voice(int i) {
+        for (int j = 0; j <= i; j++) {
+            voice();
+        }
+        System.out.println();
+    }
+
+    public void voice(int i, String word) {
+        for (int j = 0; j <= i; j++) {
+            System.out.print(word + " ");
+        }
+        System.out.println();
+    }
 }
